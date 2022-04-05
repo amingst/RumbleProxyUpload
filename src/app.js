ed3ca396ab9f820b67a39af45d8815560b6b81a7
Andrew Mingst <amingst97@gmail.com>
2022-04-05 18:36:07 -0400

    added https

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const path = require('path')
+const http2 = require('http2')
+const http2Express = require('http2-express-bridge')
+const { readFileSync } = require('fs')
 
-const app = express()
+const app = http2Express(express)
 const port = process.env.PORT || 8000
 
 app.use(express.static(path.join(__dirname, 'public')))
@@ -10,4 +13,12 @@ app.get('/', (req, res) => {
 	res.sendFile('index.html')
 })
 
-app.listen(port, () => console.log(`Server listening on port ${port}`))
+const options = {
+	key: readFileSync('KEY'),
+	cert: readFileSync('CERT'),
+	allowHTTP1: true,
+}
+
+const server = http2.createSecureServer(options, app)
+//app.listen(port, () => console.log(`Server listening on port ${port}`))
+server.listen(port)
